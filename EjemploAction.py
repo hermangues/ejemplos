0fa3d7db50518f72a2e2733ac3c5a2b87c00dea3
hermangues <157658192+hermangues@users.noreply.github.com>
2024-01-31 17:46:00 -0600

    Update EjemploAction.py

--- EjemploAction.py
+++ EjemploAction.py
@@ -1,10 +1,2 @@
-
-texto = input ("dame un texto ")
-repeticiones = input("cuantas veces se repetira: ")
-
-salida = ""
-for i in range(int(repeticiones)):
-	salida += texto
-
-print(f"::set-output name=output_text::{salida}")
+print ("Hola Mundo!!!!")
 
